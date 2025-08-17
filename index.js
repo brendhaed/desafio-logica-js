e38d8bb99348504fa486bdf2de0b2438dd36ece1
Brendha Eduarda Rodrigues <136868276+brendhaed@users.noreply.github.com>
2025-08-16 21:31:37 -0300

    envio do desafio

--- index.js
+++ index.js
@@ -1,2 +1,26 @@
-console.log("testando")
-console.log("testando2")
+// Desafio proposto: Classificador de nível de Herói
+
+let nomeHeroi = "Batman"
+let qtdXP = 1150
+let nivelHeroi = ""
+
+// estrutura de decisão do personagem
+if (qtdXP <=1000) {
+    nivelHeroi = "Ferro"
+} else if(qtdXP >= 1001 && qtdXP <= 2000) {
+    nivelHeroi = "Bronze"
+} else if(qtdXP >= 2001 && qtdXP <= 5000) {
+    nivelHeroi = "Prata"
+} else if (qtdXP >= 5001 && qtdXP <= 7000) {
+    nivelHeroi = "Ouro"
+} else if (qtdXP >= 7001 && qtdXP <= 8000) {
+    nivelHeroi = "Platina"
+} else if (qtdXP >= 8001 && qtdXP <= 9000) {
+    nivelHeroi = "Ascendente"
+} else if(qtdXP >= 9001 && 10000) {
+    nivelHeroi = "Imortal"
+} else (nivelHeroi = "Radiante")
+
+
+// mensagem final
+console.log("O herói de nome " + nomeHeroi + " está no nível de " + nivelHeroi )
